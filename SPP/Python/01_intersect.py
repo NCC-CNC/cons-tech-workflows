22b434fdee7113bcb1a70b3b449dcd15f584dc18
Dan Wismer <Daniel.Wismer@natureconservancy.ca>
2023-10-02 15:39:45 -0600

    updated parcel data

--- SPP/Python/01_intersect.py
+++ SPP/Python/01_intersect.py
@@ -1,23 +1,26 @@
 #
 # Authors: Dan Wismer
 #
-# Date: Sept 27th, 2023
+# Date: Oct 2nd, 2023
 #
 # Description: interect ECCC SAR with NCC Achievments
 #
-# Inputs:  1. NCC Achievments
+# Inputs:  1. NCC Achievments - pulled from AGOL?
 #          2. ECCC SAR Range Map Extents
 #
 # Outputs: 1. intersection of NCC and SAR
-#          2. dissolved intersection by property id
+#          2. dissolved intersection by LIS_PARCEL_ID
 #
 #===============================================================================
 
 import arcpy
 
 # Read-in source data
-NCC = "C:/Data/NAT/NCC_Accomplishments/2023_09_22_request_for_Dan/Accomplishments_pull.gdb/GIS_NCC_Accomplishments_20220913"
+# NCC = "C:/Data/NAT/NCC_Accomplishments/2023_09_22_request_for_Dan/Accomplishments_pull.gdb/GIS_NCC_Accomplishments_20220913"
+NCC = "C:/Data/NAT/NCC_Accomplishments/Accomplishments_20230928.gdb/Accomplishments_20230928"
 ECCC_SAR = "C:/Data/NAT/ECCC/SAR/Species at Risk Range Map Extents.gdb/SpeciesAtRiskRangeMapExtents"
+FGDB = "C:/Data/ANALYSIS/CHARITY_INTELLIEGENCE/MyProject.gdb"
+OUT_CSV = "C:/Data/ANALYSIS/CHARITY_INTELLIEGENCE/Output"
 
 # Set environments
 arcpy.env.workspace = "C:/Data/ANALYSIS/CHARITY_INTELLIEGENCE"
@@ -26,19 +29,19 @@ arcpy.env.overwriteOutput = True
 
 # Project ECCC SAR
 print("projecting ...")
-eccc_sar = arcpy.Project_management(ECCC_SAR , "MyProject.gdb/eccc_sar_prj", crs)
+eccc_sar = arcpy.Project_management(ECCC_SAR , "{}/eccc_sar_prj".format(FGDB), crs)
 
 # Intersect
-print("intersecting THIS TAKES A LONG TIME...")
-i = arcpy.analysis.Intersect([NCC, eccc_sar], "MyProject.gdb/ncc_eccc_sar")
+print("intersecting THIS TAKES APPROX 9 MINS...")
+i = arcpy.analysis.Intersect([NCC, eccc_sar], "{}/ncc_eccc_sar".format(FGDB))
 
 # Export attribute table as csv
 print("exporting ...")
-arcpy.conversion.ExportTable(i, "Output/NCC_ECCC_SAR.csv")
+arcpy.conversion.ExportTable(i, "{}/NCC_ECCC_SAR.csv".format(OUT_CSV))
 
-# Dissolve by Property_ID
+# Dissolve by LIS_PARCEL_ID
 print("dissolving ...")
-d = arcpy.management.Dissolve(i, "MyProject.gdb/ncc_eccc_sar_dis_prp_id", "Property_ID")
+d = arcpy.management.Dissolve(i, "{}/ncc_eccc_sar_dis_parcel".format(FGDB), ["LIS_PARCEL_ID"])
 
 # Add Area HA field
 arcpy.management.AddField(d,"SAR_HA","DOUBLE")
@@ -49,4 +52,4 @@ with arcpy.da.UpdateCursor(d, ["SAR_HA", "SHAPE@AREA"]) as cursor:
 
 # Export attribute table as csv
 print("exporting ...")
-arcpy.conversion.ExportTable(d, "Output/NCC_ECCC_SAR_DIS_PRP_ID.csv")
+arcpy.conversion.ExportTable(d, "{}/NCC_ECCC_SAR_DIS_PARCEL.csv".format(OUT_CSV))
--- SPP/R/01_sar.R
+++ SPP/R/01_sar.R
@@ -1,7 +1,7 @@
 #
 # Authors: Dan Wismer
 #
-# Date: Sept 27th, 2023
+# Date: Oct 2nd, 2023
 #
 # Description: Summaries unique species names, count and area (overlap dissolved)
 #              regionally and nationally
@@ -17,225 +17,101 @@
 library(dplyr)
 library(lubridate)
 
+OUTPUT <- "C:/Data/ANALYSIS/CHARITY_INTELLIEGENCE/Output"
+
 # Read-in ECCC SAR NCC csv
-sar_csv <- read.csv("SPP/Output/NCC_ECCC_SAR.csv", encoding = "UTF-8")
-sar_csv$FIRST_NCC_DATE <- as.Date(sar_csv$FIRST_NCC_DATE, format = "%Y-%m-%d")
-prp_csv <- read.csv("SP/Output/NCC_ECCC_SAR_DIS_PRP_ID.csv", encoding = "UTF-8")
+sar_csv <- read.csv(paste0(OUTPUT, "/NCC_ECCC_SAR.csv"), encoding = "UTF-8")
+sar_csv$PCLISUM_NCC_INT_FRST_START_DATE <- as.Date(sar_csv$PCLISUM_NCC_INT_FRST_START_DATE, format = "%Y-%m-%d")
+parcel_csv <- read.csv(paste0(OUTPUT, "/NCC_ECCC_SAR_DIS_PARCEL.csv"), encoding = "UTF-8")
 
 # Join csv
-sar_df <- left_join(sar_csv, prp_csv, by = "Property_ID")
+sar_df <- left_join(sar_csv, parcel_csv, by = "LIS_PARCEL_ID")
 
-# Include only these fields:
-fields <- c(
-  "Region", "LIS_PARCEL_ID", "Property_ID", "Parcel_Interest", "Property_Name", 
-  "ACTIVE","FIRST_NCC_DATE", "COSEWICID", "SCI_NAME", "COM_NAME_E", "SAR_HA"
-)
+# Regional HA function ----
+get_reg_ha_tbl <- function(yyyymmdd){
+  sar_df %>%
+    filter(ymd(PCLISUM_NCC_INT_FRST_START_DATE) <= ymd(yyyymmdd)) %>%
+    group_by(LIS_PARCEL_ID, PCL_REGION_EN) %>%
+    summarise(SAR_HA = first(SAR_HA)) %>%
+    group_by(PCL_REGION_EN) %>%
+    summarise(SAR_HA = sum(SAR_HA))
+}
+
+# NAT table function ----
+get_nat_tbl <- function(yyyymmdd, regional_ha_tbl) {
+  sar_df %>%
+    filter(ymd(PCLISUM_NCC_INT_FRST_START_DATE) <= ymd(yyyymmdd)) %>%
+    summarise(
+      DATE = paste0("<= ", yyyymmdd),
+      SAR_NAME = paste(unique(COM_NAME_E), collapse = ", "),
+      SAR_COUNT = length(unique(COM_NAME_E))
+    ) %>%
+    mutate("SAR_HA" = sum(regional_ha_tbl$SAR_HA))
+}
+
+# REG table function ----
+get_reg_tbl <- function(yyyymmdd, regional_ha_tbl) {
+  sar_df %>%
+    group_by(PCL_REGION_EN) %>%
+    summarise(
+      DATE = paste0("<= ", yyyymmdd),
+      SAR_NAME = paste(unique(COM_NAME_E), collapse = ", "),
+      SAR_COUNT = length(unique(COM_NAME_E))
+    ) %>%
+    mutate("SAR_HA" = regional_ha_tbl$SAR_HA)
+}
+
 
 # 2018 ---
-## HA
-sar_ha_df_lte2018 <- sar_df %>%
-  select(all_of(fields)) %>%
-  filter(ymd(FIRST_NCC_DATE) <= ymd("2018-12-31")) %>%
-  group_by(Property_ID, Region) %>%
-  summarise(
-    SAR_HA = first(SAR_HA),
-  ) %>%
-  group_by(Region) %>%
-  summarise(
-    SAR_HA = sum(SAR_HA)
-  )
+ha_lte2018 <- get_reg_ha_tbl("2018-12-31") 
+nat_lte2018 <- get_nat_tbl("2018-12-31", ha_lte2018) 
+reg_lte2018 <- get_reg_tbl("2018-12-31", ha_lte2018)
 
-## NAT 
-ncc_nat_sar_lte2018 <- sar_df %>%
-  select(all_of(fields)) %>%
-  filter(ymd(FIRST_NCC_DATE) <= ymd("2018-12-31")) %>%
-  summarise(
-    DATE = "<= 2018-12-31",
-    SAR_NAME = paste(unique(COM_NAME_E), collapse = ", "),
-    SAR_COUNT = length(unique(COM_NAME_E))
-  ) %>%
-  mutate("SAR_HA" = prettyNum(sum(sar_ha_df_lte2018$SAR_HA), big.mark = ","))
+# 2019 ---
+ha_lte2019 <- get_reg_ha_tbl("2019-12-31") 
+nat_lte2019 <- get_nat_tbl("2019-12-31", ha_lte2019) 
+reg_lte2019 <- get_reg_tbl("2019-12-31", ha_lte2019)
 
-## REG
-ncc_region_sar_lte2018 <- sar_df %>%
-  select(all_of(fields)) %>%
-  filter(ymd(FIRST_NCC_DATE) <= ymd("2018-12-31")) %>%
-  group_by(Region) %>%
-  summarise(
-    DATE = "<= 2018-12-31",
-    SAR_NAME = paste(unique(COM_NAME_E), collapse = ", "),
-    SAR_COUNT = length(unique(COM_NAME_E))
-  ) %>%
-  mutate("SAR_HA" = prettyNum(sar_ha_df_lte2018$SAR_HA, big.mark = ","))
+# 2020 ---
+ha_lte2020 <- get_reg_ha_tbl("2020-12-31") 
+nat_lte2020 <- get_nat_tbl("2020-12-31", ha_lte2020) 
+reg_lte2020 <- get_reg_tbl("2020-12-31", ha_lte2020)
 
+# 2021 ---
+ha_lte2021 <- get_reg_ha_tbl("2021-12-31") 
+nat_lte2021 <- get_nat_tbl("2021-12-31", ha_lte2021) 
+reg_lte2021 <- get_reg_tbl("2021-12-31", ha_lte2021)
 
-# 2019 ----
-# HA
-sar_ha_df_lte2019 <- sar_df %>%
-  select(all_of(fields)) %>%
-  filter(ymd(FIRST_NCC_DATE) <= ymd("2019-12-31")) %>%
-  group_by(Property_ID, Region) %>%
-  summarise(
-    SAR_HA = first(SAR_HA),
-  ) %>%
-  group_by(Region) %>%
-  summarise(
-    SAR_HA = sum(SAR_HA)
-  )
+# 2022 ---
+ha_lte2022 <- get_reg_ha_tbl("2022-12-31") 
+nat_lte2022 <- get_nat_tbl("2022-12-31", ha_lte2022) 
+reg_lte2022 <- get_reg_tbl("2022-12-31", ha_lte2022)
 
-## NAT 
-ncc_nat_sar_lte2019 <- sar_df %>%
-  select(all_of(fields)) %>%
-  filter(ymd(FIRST_NCC_DATE) <= ymd("2019-12-31")) %>%
-  summarise(
-    DATE = "<= 2019-12-31",
-    SAR_NAME = paste(unique(COM_NAME_E), collapse = ", "),
-    SAR_COUNT = length(unique(COM_NAME_E))
-  ) %>%
-  mutate("SAR_HA" = prettyNum(sum(sar_ha_df_lte2019$SAR_HA), big.mark = ","))
-
-## REG
-ncc_region_sar_lte2019 <- sar_df %>%
-  select(all_of(fields)) %>%
-  filter(ymd(FIRST_NCC_DATE) <= ymd("2019-12-31")) %>%
-  group_by(Region) %>%
-  summarise(
-    DATE = "<= 2019-12-31",
-    SAR_NAME = paste(unique(COM_NAME_E), collapse = ", "),
-    SAR_COUNT = length(unique(COM_NAME_E))
-  ) %>%
-  mutate("SAR_HA" = prettyNum(sar_ha_df_lte2019$SAR_HA, big.mark = ","))
-
-# 2020 ---- 
-## HA
-sar_ha_df_lte2020 <- sar_df %>%
-  select(all_of(fields)) %>%
-  filter(ymd(FIRST_NCC_DATE) <= ymd("2020-12-31")) %>%
-  group_by(Property_ID, Region) %>%
-  summarise(
-    SAR_HA = first(SAR_HA),
-  ) %>%
-  group_by(Region) %>%
-  summarise(
-    SAR_HA = sum(SAR_HA)
-  )
-
-## NAT 
-ncc_nat_sar_lte2020 <- sar_df %>%
-  select(all_of(fields)) %>%
-  filter(ymd(FIRST_NCC_DATE) <= ymd("2020-12-31")) %>%
-  summarise(
-    DATE = "<= 2020-12-31",
-    SAR_NAME = paste(unique(COM_NAME_E), collapse = ", "),
-    SAR_COUNT = length(unique(COM_NAME_E))
-  ) %>%
-  mutate("SAR_HA" = prettyNum(sum(sar_ha_df_lte2020$SAR_HA), big.mark = ","))
-
-## REG
-ncc_region_sar_lte2020 <- sar_df %>%
-  select(all_of(fields)) %>%
-  filter(ymd(FIRST_NCC_DATE) <= ymd("2020-12-31")) %>%
-  group_by(Region) %>%
-  summarise(
-    DATE = "<= 2020-12-31",
-    SAR_NAME = paste(unique(COM_NAME_E), collapse = ", "),
-    SAR_COUNT = length(unique(COM_NAME_E))
-  ) %>%
-  mutate("SAR_HA" = prettyNum(sar_ha_df_lte2020$SAR_HA, big.mark = ","))
-
-# 2021 
-## HA
-sar_ha_df_lte2021 <- sar_df %>%
-  select(all_of(fields)) %>%
-  filter(ymd(FIRST_NCC_DATE) <= ymd("2021-12-31")) %>%
-  group_by(Property_ID, Region) %>%
-  summarise(
-    SAR_HA = first(SAR_HA),
-  ) %>%
-  group_by(Region) %>%
-  summarise(
-    SAR_HA = sum(SAR_HA)
-  )
-
-## NAT 
-ncc_nat_sar_lte2021 <- sar_df %>%
-  select(all_of(fields)) %>%
-  filter(ymd(FIRST_NCC_DATE) <= ymd("2021-12-31")) %>%
-  summarise(
-    DATE = "<= 2021-12-31",
-    SAR_NAME = paste(unique(COM_NAME_E), collapse = ", "),
-    SAR_COUNT = length(unique(COM_NAME_E))
-  ) %>%
-  mutate("SAR_HA" = prettyNum(sum(sar_ha_df_lte2021$SAR_HA), big.mark = ","))
-
-## REG
-ncc_region_sar_lte2021 <- sar_df %>%
-  select(all_of(fields)) %>%
-  filter(ymd(FIRST_NCC_DATE) <= ymd("2021-12-31")) %>%
-  group_by(Region) %>%
-  summarise(
-    DATE = "<= 2021-12-31",
-    SAR_NAME = paste(unique(COM_NAME_E), collapse = ", "),
-    SAR_COUNT = length(unique(COM_NAME_E))
-  ) %>%
-  mutate("SAR_HA" = prettyNum(sar_ha_df_lte2021$SAR_HA, big.mark = ","))
-
-# 2022
-## HA
-sar_ha_df_lte2022 <- sar_df %>%
-  select(all_of(fields)) %>%
-  filter(ymd(FIRST_NCC_DATE) <= ymd("2022-12-31")) %>%
-  group_by(Property_ID, Region) %>%
-  summarise(
-    SAR_HA = first(SAR_HA),
-  ) %>%
-  group_by(Region) %>%
-  summarise(
-    SAR_HA = sum(SAR_HA)
-  )
-
-## NAT
-ncc_nat_sar_lte2022 <- sar_df %>%
-  select(all_of(fields)) %>%
-  filter(ymd(FIRST_NCC_DATE) <= ymd("2022-12-31")) %>%
-  summarise(
-    DATE = "<= 2022-12-31",
-    SAR_NAME = paste(unique(COM_NAME_E), collapse = ", "),
-    SAR_COUNT = length(unique(COM_NAME_E))
-  ) %>%
-  mutate("SAR_HA" = prettyNum(sum(sar_ha_df_lte2022$SAR_HA), big.mark = ","))
-
-## REF
-ncc_region_sar_lte2022 <- sar_df %>%
-  select(all_of(fields)) %>%
-  filter(ymd(FIRST_NCC_DATE) <= ymd("2022-12-31")) %>%
-  group_by(Region) %>%
-  summarise(
-    DATE = "<= 2022-12-31",
-    SAR_NAME = paste(unique(COM_NAME_E), collapse = ", "),
-    SAR_COUNT = length(unique(COM_NAME_E))
-  ) %>%
-  mutate("SAR_HA" = prettyNum(sar_ha_df_lte2022$SAR_HA, big.mark = ","))
-
-
+# 2023 ---
+ha_lte2023 <- get_reg_ha_tbl("2023-12-31") 
+nat_lte2023 <- get_nat_tbl("2023-12-31", ha_lte2023) 
+reg_lte2023 <- get_reg_tbl("2023-12-31", ha_lte2023)
 # Merge tables ----
 ## NAT
 ncc_5yr_sar_nat <- rbind(
-  ncc_nat_sar_lte2018, 
-  ncc_nat_sar_lte2019,
-  ncc_nat_sar_lte2020,
-  ncc_nat_sar_lte2021,
-  ncc_nat_sar_lte2022
+  nat_lte2018, 
+  nat_lte2019,
+  nat_lte2020,
+  nat_lte2021,
+  nat_lte2022,
+  nat_lte2023
   )
-write.csv(ncc_5yr_sar_nat, "SPP/Output/NCC_5YR_SAR_NAT.csv")
+write.csv(ncc_5yr_sar_nat, file = paste0(OUTPUT, "/NCC_5YR_SAR_NAT.csv"), fileEncoding = "ISO-8859-1", row.names = FALSE)
 
 ## REG
 ncc_5yr_sar_by_region <- rbind(
-  ncc_region_sar_lte2018, 
-  ncc_region_sar_lte2019,
-  ncc_region_sar_lte2020,
-  ncc_region_sar_lte2021,
-  ncc_region_sar_lte2022
+  reg_lte2018, 
+  reg_lte2019,
+  reg_lte2020,
+  reg_lte2021,
+  reg_lte2022,
+  reg_lte2023
   ) %>%
+  rename(Region = PCL_REGION_EN) %>%
   arrange(Region)
-write.csv(ncc_5yr_sar_by_region, "SPP/Output/NCC_5YR_SAR_REG.csv")
+write.csv(ncc_5yr_sar_by_region, file = paste0(OUTPUT,  "/NCC_5YR_SAR_REG.csv"), fileEncoding = "ISO-8859-1", row.names = FALSE)
